adadda81660579d16dd8b05a9e2057fff27a0e7e
Robin Dahiya <robindahiya@Robins-MacBook-Air.local>
2021-08-02 23:43:10 +0530

    done

--- src/redux/service/loginService.js
+++ src/redux/service/loginService.js
@@ -3,21 +3,21 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const studentLoginApi = createAsyncThunk('student/loginApi', async (data) => {
     // return await Axios.post('http://localhost:9000/api/student/login', data);
-    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com//api/student/login', data);
+    return await Axios.post('https://api.myonlineedu.in/api/student/login', data);
     // return await Axios.post('https://www.myonlineedu.in/api/student/login', data);
 })
 export const teacherLoginApi = createAsyncThunk('teacher/teacherLoginSlice', async (data) => {
     // return await Axios.post('https://localhost:9000/api/teacher/login', data);
-    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com//api/teacher/login', data);
+    return await Axios.post('https://api.myonlineedu.in/api/teacher/login', data);
     // return await Axios.post('https://www.myonlineedu.in/api/teacher/login', data);
 })
 export const getStudentData = createAsyncThunk('student/studentData', async(data) => {
     // return await Axios.post('https://localhost:9000/api/student/data', data);
-    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com//api/student/data', data);
+    return await Axios.post('https://api.myonlineedu.in/api/student/data', data);
     // return await Axios.post('https://www.myonlineedu.in/api/student/data', data);
 })
 export const getTeacherData = createAsyncThunk('teacher/teacherData', async(data) => {
     // return await Axios.post('https://localhost:9000/api/teacher/data', data);
-    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com//api/teacher/data', data);
+    return await Axios.post('https://api.myonlineedu.in/api/teacher/data', data);
     // return await Axios.post('https://www.myonlineedu.in/api/teacher/data', data);
 })
--- src/redux/service/teacherService.js
+++ src/redux/service/teacherService.js
@@ -9,7 +9,7 @@ const headers = {
     'auth_token': localStorage.getItem('auth_token'),
 };
 export const getTeacherDashboard = async (body) => {
-    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com//api/teacher/dashboard?userName=${body.userName}`, {
+    return await Axios.get(`https://api.myonlineedu.in//api/teacher/dashboard?userName=${body.userName}`, {
         headers: {
             'auth_token': localStorage.getItem('auth_token')
           }
@@ -24,29 +24,29 @@ export const getTeacherDashboard = async (body) => {
 // export const assignQuestionsToStudents = async (body) => {
 //     console.log(body, "@@##");
 //     console.log("hii")
-//     return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com///api/teacher/assign`, body, {headers})
+//     return await Axios.put(`https://api.myonlineedu.in///api/teacher/assign`, body, {headers})
 // }
 
 
 
 export const assignQuestionsToStudents = createAsyncThunk('teacher/assign', async (data) => {
     // console.log(data, 'data')
-    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    return await Axios.put(`https://api.myonlineedu.in/api/teacher/assign`, data, {headers})
     // return await Axios.put(`http://localhost:9000/api/teacher/assign`, data, {headers})
 })
 
 export const getStudentOfTeacher = async (data) => {
     const {className, userName} = data;
-    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    return await Axios.put(`https://api.myonlineedu.in/api/teacher/assign`, data, {headers})
     // return await Axios.get(`http://localhost:9000/api/teacher/allstudent?className=${className}&userName=${userName}`)
 }
 export const getStudentResult = async (data) => {
     const {userName, teacherUserName, subject, chapter} = data;
-    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    return await Axios.put(`https://api.myonlineedu.in/api/teacher/assign`, data, {headers})
     // return await Axios.get(`http://localhost:9000/api/teacher/student/result?userName=${userName}&selectedSubjects=${subject}&selectedChapter=${chapter}`)
 }
 export const getStudentSubject = async (data) => {
     const {userName} = data;
-    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    return await Axios.put(`https://api.myonlineedu.in/api/teacher/assign`, data, {headers})
     // return await Axios.get(`http://localhost:9000/api/teacher/student/subject?userName=${userName}`)
 }
