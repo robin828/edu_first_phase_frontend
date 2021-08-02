da95a32354a87bb885acdd6311d948de56f46c7e
Robin Dahiya <robindahiya@Robins-MacBook-Air.local>
2021-08-02 22:35:10 +0530

    deploying

--- src/redux/service/studentService.js
+++ src/redux/service/studentService.js
@@ -2,53 +2,54 @@ import Axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const getStudentDashboard = async () => {
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/dashboard?userName=${localStorage.getItem('studentUsername')}`);
-    return await Axios.get(`http://localhost:9000/api/student/dashboard?userName=${localStorage.getItem('studentUsername')}`);
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/dashboard?userName=${localStorage.getItem('studentUsername')}`);
+    // return await Axios.get(`http://localhost:9000/api/student/dashboard?userName=${localStorage.getItem('studentUsername')}`);
 }
 export const getStudentTopCard = async () => {
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/gettopcard?userName=${localStorage.getItem('studentUsername')}`);
-    return await Axios.get(`http://localhost:9000/api/student/gettopcard?userName=${localStorage.getItem('studentUsername')}`);
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/gettopcard?userName=${localStorage.getItem('studentUsername')}`);
+    // return await Axios.get(`http://localhost:9000/api/student/gettopcard?userName=${localStorage.getItem('studentUsername')}`);
 }
 
 export const getStudentClass = createAsyncThunk('student/studentClass', async (data) => {
-    // return await Axios.post('http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/class', data);
-    return await Axios.post('http://localhost:9000/api/student/class', data);
+    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/class', data);
+    // return await Axios.post('http://localhost:9000/api/student/class', data);
 })
 export const getExam = async (className) => {
-    return await Axios.get(`http://localhost:9000/api/getexam?className=${className}`);
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/getexam?className=${className}`);
+    // return await Axios.get(`http://localhost:9000/api/getexam?className=${className}`);
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/getexam?className=${className}`);
 }
 export const getChapters = async (subjectName) => {
-    return await Axios.get(`http://localhost:9000/api/getchapter?subjectName=${subjectName}`);
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/getchapter?subjectName=${subjectName}`);
+    // return await Axios.get(`http://localhost:9000/api/getchapter?subjectName=${subjectName}`);
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/getchapter?subjectName=${subjectName}`);
 }
 export const getResults = async () => {
-    return await Axios.get(`http://localhost:9000/api/student/getresults?userName=${localStorage.getItem('studentUsername')}`);
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/getresults?userName=${localStorage.getItem('studentUsername')}`);
+    // return await Axios.get(`http://localhost:9000/api/student/getresults?userName=${localStorage.getItem('studentUsername')}`);
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/getresults?userName=${localStorage.getItem('studentUsername')}`);
 }
 export const getTeacherQuestions = async () => {
-    return await Axios.get(`http://localhost:9000/api/student/teacher/question?userName=${localStorage.getItem('studentUsername')}`);
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/teacher/question?userName=${localStorage.getItem('studentUsername')}`);
+    // return await Axios.get(`http://localhost:9000/api/student/teacher/question?userName=${localStorage.getItem('studentUsername')}`);
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/teacher/question?userName=${localStorage.getItem('studentUsername')}`);
 }
 
 export const getChapterWisePerformance = async ({userName, selectedChapter, selectedExam, selectedSubject}) => {
-    return await Axios.get(`http://localhost:9000/api/student/performance/chapterwise?userName=${localStorage.getItem('studentUsername')}&exam=${selectedExam}&subject=${selectedSubject}&chapter=${selectedChapter}&userName=${userName}`);
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/performance/chapterwise?userName=${localStorage.getItem('studentUsername')}&exam=${selectedExam}&subject=${selectedSubject}&chapter=${selectedChapter}`);
+    // return await Axios.get(`http://localhost:9000/api/student/performance/chapterwise?userName=${localStorage.getItem('studentUsername')}&exam=${selectedExam}&subject=${selectedSubject}&chapter=${selectedChapter}&userName=${userName}`);
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/performance/chapterwise?userName=${localStorage.getItem('studentUsername')}&exam=${selectedExam}&subject=${selectedSubject}&chapter=${selectedChapter}`);
 }
 
 export const getQuestions = createAsyncThunk('student/question', async (data) => {
     // console.log(data, 'data')
-    return await Axios.post('http://localhost:9000/api/student/question', data);
-    // return await Axios.post('http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/question', data);
+    // return await Axios.post('http://localhost:9000/api/student/question', data);
+    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/question', data);
 })
 
 export const getQuestionBy_id = createAsyncThunk('student/question', async (data) => {
     // console.log(data, 'data')
-    return await Axios.post('http://localhost:9000/api/student/result/question', data);
-    // return await Axios.post('http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/student/result/question', data);
+    // return await Axios.post('http://localhost:9000/api/student/result/question', data);
+    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/result/question', data);
 })
 
 export const saveResult = createAsyncThunk('student/question', async (data) => {
     // console.log(data, 'data')
-    return await Axios.post('http://localhost:9000/api/student/saveresult', data);
+    // return await Axios.post('http://localhost:9000/api/student/saveresult', data);
+    return await Axios.post('http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/student/saveresult', data);
 })
--- src/redux/service/teacherService.js
+++ src/redux/service/teacherService.js
@@ -9,44 +9,44 @@ const headers = {
     'auth_token': localStorage.getItem('auth_token'),
 };
 export const getTeacherDashboard = async (body) => {
-    // return await Axios.get(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/teacher/dashboard?userName=${body.userName}`, {
-    //     headers: {
-    //         'auth_token': localStorage.getItem('auth_token')
-    //       }
-    // });
-    return await Axios.get(`http://localhost:9000/api/teacher/dashboard?userName=${body.userName}`, {
+    return await Axios.get(`http://myonlineedu.ap-south-1.elasticbeanstalk.com//api/teacher/dashboard?userName=${body.userName}`, {
         headers: {
             'auth_token': localStorage.getItem('auth_token')
           }
     });
+    // return await Axios.get(`http://localhost:9000/api/teacher/dashboard?userName=${body.userName}`, {
+    //     headers: {
+    //         'auth_token': localStorage.getItem('auth_token')
+    //       }
+    // });
 }
 
 // export const assignQuestionsToStudents = async (body) => {
 //     console.log(body, "@@##");
 //     console.log("hii")
-//     return await Axios.put(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com//api/teacher/assign`, body, {headers})
+//     return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com///api/teacher/assign`, body, {headers})
 // }
 
 
 
 export const assignQuestionsToStudents = createAsyncThunk('teacher/assign', async (data) => {
     // console.log(data, 'data')
-    // return await Axios.put(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
-    return await Axios.put(`http://localhost:9000/api/teacher/assign`, data, {headers})
+    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    // return await Axios.put(`http://localhost:9000/api/teacher/assign`, data, {headers})
 })
 
 export const getStudentOfTeacher = async (data) => {
     const {className, userName} = data;
-    // return await Axios.put(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
-    return await Axios.get(`http://localhost:9000/api/teacher/allstudent?className=${className}&userName=${userName}`)
+    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    // return await Axios.get(`http://localhost:9000/api/teacher/allstudent?className=${className}&userName=${userName}`)
 }
 export const getStudentResult = async (data) => {
     const {userName, teacherUserName, subject, chapter} = data;
-    // return await Axios.put(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
-    return await Axios.get(`http://localhost:9000/api/teacher/student/result?userName=${userName}&selectedSubjects=${subject}&selectedChapter=${chapter}`)
+    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    // return await Axios.get(`http://localhost:9000/api/teacher/student/result?userName=${userName}&selectedSubjects=${subject}&selectedChapter=${chapter}`)
 }
 export const getStudentSubject = async (data) => {
     const {userName} = data;
-    // return await Axios.put(`http://eduprodfirstphasebackend-env.eba-fi5wgagu.us-east-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
-    return await Axios.get(`http://localhost:9000/api/teacher/student/subject?userName=${userName}`)
+    return await Axios.put(`http://myonlineedu.ap-south-1.elasticbeanstalk.com/api/teacher/assign`, data, {headers})
+    // return await Axios.get(`http://localhost:9000/api/teacher/student/subject?userName=${userName}`)
 }
