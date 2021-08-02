118f7b5ef3d6aa9d4d7b365b91043d8c56a1f5e0
Robin Dahiya <robindahiya@Robins-MacBook-Air.local>
2021-08-03 00:59:32 +0530

    working

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { loginSlice } from './slice/loginSlice';
 import { teacherLoginSlice } from './slice/teacherLoginSlice';
-import { studentDataSlice } from './slice/studentDataSlice';
-import { teacherDataSlice } from './slice/teacherDataSlice';
+// import { studentDataSlice } from './slice/studentDataSlice';
+// import { teacherDataSlice } from './slice/teacherDataSlice';
 import { announcementSlice } from './slice/announcementSlice';
 import { questionSlice } from './slice/questionSlice';
 
@@ -10,8 +10,8 @@ const store = configureStore({
     reducer: {
         userLogin: loginSlice.reducer,
         teacherLogin: teacherLoginSlice.reducer,
-        studentData: studentDataSlice.reducer,
-        teacherData: teacherDataSlice.reducer,
+        // studentData: studentDataSlice.reducer,
+        // teacherData: teacherDataSlice.reducer,
         announcement: announcementSlice.reducer,
         questions: questionSlice.reducer
     },
