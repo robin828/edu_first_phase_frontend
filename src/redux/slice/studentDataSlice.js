7c67a63928c3a101a3a0226e164f8a4cca3e59a4
Robin Dahiya <robindahiya@Robins-MacBook-Air.local>
2021-08-03 00:54:39 +0530

    working

--- src/redux/slice/studentDataSlice.js
+++ src/redux/slice/studentDataSlice.js
@@ -1,25 +1,25 @@
-import {createSlice} from '@reduxjs/toolkit';
-import { getStudentData } from '../service/loginService'
+// import {createSlice} from '@reduxjs/toolkit';
+// import { getStudentData } from '../service/loginService'
 
-export const studentDataSlice = createSlice({
-    name: 'student',
-    initialState: {
-        login: {},
-        loading: true,
-        error:"",
-    },
-    reducers: {
+// export const studentDataSlice = createSlice({
+//     name: 'student',
+//     initialState: {
+//         login: {},
+//         loading: true,
+//         error:"",
+//     },
+//     reducers: {
 
-    },
-    extraReducers:(builder) => {
-        builder.addCase(getStudentData.fulfilled, (state, action) => {
-            state.login = {...action.payload.data};
-        })
-        builder.addCase(getStudentData.pending, (state) => {
-            state.loading = true;
-        })
-        builder.addCase(getStudentData.rejected, (state, action) => {
-            state.error = action.error.message;
-        })
-    }
-})
+//     },
+//     extraReducers:(builder) => {
+//         builder.addCase(getStudentData.fulfilled, (state, action) => {
+//             state.login = {...action.payload.data};
+//         })
+//         builder.addCase(getStudentData.pending, (state) => {
+//             state.loading = true;
+//         })
+//         builder.addCase(getStudentData.rejected, (state, action) => {
+//             state.error = action.error.message;
+//         })
+//     }
+// })
--- src/redux/slice/teacherDataSlice.js
+++ src/redux/slice/teacherDataSlice.js
@@ -1,28 +1,28 @@
-import {createSlice} from '@reduxjs/toolkit';
-import { getTeacherData } from '../service/loginService'
+// import {createSlice} from '@reduxjs/toolkit';
+// import { getTeacherData } from '../service/loginService'
 
-export const teacherDataSlice = createSlice({
-    name: 'teacher',
-    initialState: {
-        standard: {},
-        loading: true,
-        error:"",
-    },
-    reducers: {
+// export const teacherDataSlice = createSlice({
+//     name: 'teacher',
+//     initialState: {
+//         standard: {},
+//         loading: true,
+//         error:"",
+//     },
+//     reducers: {
 
-    },
-    extraReducers:(builder) => {
-        builder.addCase(getTeacherData.fulfilled, (state, action) => {
-            console.log(state)
-            console.log("state")
+//     },
+//     extraReducers:(builder) => {
+//         builder.addCase(getTeacherData.fulfilled, (state, action) => {
+//             console.log(state)
+//             console.log("state")
 
-            state.login = {...action.payload.data};
-        })
-        builder.addCase(getTeacherData.pending, (state) => {
-            state.loading = true;
-        })
-        builder.addCase(getTeacherData.rejected, (state, action) => {
-            state.error = action.error.message;
-        })
-    }
-})
+//             state.login = {...action.payload.data};
+//         })
+//         builder.addCase(getTeacherData.pending, (state) => {
+//             state.loading = true;
+//         })
+//         builder.addCase(getTeacherData.rejected, (state, action) => {
+//             state.error = action.error.message;
+//         })
+//     }
+// })
