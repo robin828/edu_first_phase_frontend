62d628fa043555f09170b2fdcf7255caac489e8d
Robin Dahiya <robindahiya@Robins-MacBook-Air.local>
2021-08-05 01:10:36 +0530

    login bug

--- src/redux/service/loginService.js
+++ src/redux/service/loginService.js
@@ -2,13 +2,13 @@ import Axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const studentLoginApi = createAsyncThunk('student/loginApi', async (data) => {
-    return await Axios.post('http://localhost:9000/api/student/login', data);
-    // return await Axios.post('https://api.myonlineedu.in/api/student/login', data);
+    // return await Axios.post('http://localhost:9000/api/student/login', data);
+    return await Axios.post('https://api.myonlineedu.in/api/student/login', data);
     // return await Axios.post('https://www.myonlineedu.in/api/student/login', data);
 })
 export const teacherLoginApi = createAsyncThunk('teacher/teacherLoginSlice', async (data) => {
-    return await Axios.post('https://localhost:9000/api/teacher/login', data);
-    // return await Axios.post('https://api.myonlineedu.in/api/teacher/login', data);
+    // return await Axios.post('https://localhost:9000/api/teacher/login', data);
+    return await Axios.post('https://api.myonlineedu.in/api/teacher/login', data);
     // return await Axios.post('https://www.myonlineedu.in/api/teacher/login', data);
 })
 // export const getStudentData = createAsyncThunk('student/studentData', async(data) => {
