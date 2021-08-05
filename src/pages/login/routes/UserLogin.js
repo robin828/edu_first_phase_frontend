6df7c0423a5ef11ecda9ad8e61dfc3798c80a180
Robin Dahiya <robindahiya@Robins-MacBook-Air.local>
2021-08-05 11:06:12 +0530

    login bug

--- src/pages/login/routes/UserLogin.js
+++ src/pages/login/routes/UserLogin.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import { useHistory } from 'react-router'
-import { useSelector } from 'react-redux';
 const UserLogin = () => {
     const history = useHistory();
-    const loggedIn = useSelector((state)=>state.login.loggedIn)
-    // React.useEffect(()=>{÷
-    // setTimeout(() => {
-    //     if(localStorage.getItem('studentUsername')) {
-    //         history.push('/student/dashboard');
-    //     }
-    //     else {
-    //         alert('Wrong id and password');
-    //         history.push('/')
-    //     }
-    // }, 2000);
-    if(loggedIn) history.push('/student/dashboard');
+    setTimeout(() => {
+        if(localStorage.getItem('studentUsername')) {
+            history.push('/student/dashboard');
+        }
+        else {
+            alert('Wrong id and password');
+            history.push('/')
+        }
+    }, 2000);
     return (
         <div>
            Loading.... 
